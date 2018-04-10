9b60b153afaf9813d47201b66ad1b2204056d576
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2018-04-10 17:45:00 -0400

    New project
    New project for an iOS game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text gameScore;
+	public Text winText;
+	private int count;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setGameScore ();
+		winText.text = "";
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			setGameScore ();
+		}
+	}
+
+	void setGameScore()
+	{
+		gameScore.text = "Count: " + count.ToString();
+		if (count == 9) 
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
