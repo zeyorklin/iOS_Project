3cbc3dc53d21da095e09b8e2661d9c25d466c7de
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2018-04-10 20:13:01 -0400

    Rotating ball
    Add a rotating ball

---
+++ Assets/Scripts/RotateBall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBall : MonoBehaviour {
+	public int rotatingSpeed = 0;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, rotatingSpeed) * Time.deltaTime);
+	}
+}
