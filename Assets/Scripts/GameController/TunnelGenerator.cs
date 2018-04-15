dc389bf84b7a0abe8b8a001c395a85671678be77
Han Chao Zhao <johnsonjohnson1992@gmail.com>
2018-04-15 17:17:53 -0400

    Revert "Revert "Clean up with mess up""
    This reverts commit 9c2fdd233581f55874e4eb64c6fabdb4fbf32240.

---
+++ Assets/Scripts/GameController/TunnelGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelGenerator : MonoBehaviour {
+    public GameObject tunnelCutout;
+	private GameObject[] tunnelCutoutArr;
+    private GameObject[] preprePhase;
+    public int preparePhaseAmount;
+	public int numTunnelCutout;
+	private Quaternion rotation;
+	private float lastPos;
+    public int maxObs;
+    public float scaleDistance;
+
+    public int generateDistance = 3;
+    // Use this for initialization
+    void Start () {
+        TunnelInitilization();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TunnelUpdate();
+	}
+
+    // Initilize tunnels
+    private void TunnelInitilization()
+    {
+        tunnelCutoutArr = new GameObject[numTunnelCutout];
+        for (int x = 0; x < numTunnelCutout; x++)
+        {
+            tunnelCutoutArr[x] = (GameObject)Instantiate(tunnelCutout, new Vector3(0, 0, 0 + x * scaleDistance), Quaternion.identity);
+
+        }
+        for (int x = numTunnelCutout / 2; x < numTunnelCutout; x+= 2)
+        {
+            int n = Random.Range(1, maxObs + 1);
+            tunnelCutoutArr[x].GetComponent<ObstacleManager>().GenerateObstacles(n);
+        }
+    }
+
+
+    // Update tunnels
+    void TunnelUpdate()
+    {
+        if (generateDistance <= 0)
+        {
+            generateDistance = 3;
+        }
+
+        for (int x = 0; x < numTunnelCutout; x++)
+        {
+            
+            // Get the index of the furtherst tunnel cutout
+            int pre = x == 0 ? numTunnelCutout - 1 : x - 1;
+            if (tunnelCutoutArr[x].transform.position.z <= -12)
+            {
+                // Disable all obstacles on this tunnel cutout
+                tunnelCutoutArr[x].GetComponent<ObstacleManager>().DestroyObstacles();
+                tunnelCutoutArr[x].transform.position = new Vector3(tunnelCutoutArr[x].transform.position.x,
+                    tunnelCutoutArr[x].transform.position.y,
+                    tunnelCutoutArr[pre].transform.position.z + scaleDistance);
+
+                // Generate obstacles randomly
+                if (generateDistance == 3)
+                {
+                    int n = Random.Range(1, maxObs + 1);
+                    tunnelCutoutArr[x].GetComponent<ObstacleManager>().GenerateObstacles(n);
+                    
+                }
+                generateDistance--;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Tunnel/Obstacles/ObstacleManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Manage the obstacles on this tunnel cutout
+public class ObstacleManager : MonoBehaviour {
+
+    // All child objet(tunnel pieces) of this tunnel cutout
+    private GameObject[] tunnelPieces;
+
+	// Use this for initialization
+	void Awake () {
+        InitializeTunnelPieces();
+	}
+	
+    // Get all the tunnel pieces.
+	private void InitializeTunnelPieces()
+    {
+        tunnelPieces = new GameObject[16];
+        for (int i = 0; i < 16; i++)
+        {
+            tunnelPieces[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Generate n obstacles randomly on this tunnel cutout
+    public void GenerateObstacles(int n)
+    {
+        for (int i = 1; i < n;)
+        {
+            int rand = Random.Range(0, 16);
+            if (!tunnelPieces[rand].GetComponent<ObstacleGenerator>().hasObs)
+            {
+                tunnelPieces[rand].GetComponent<ObstacleGenerator>().ActivateObs();
+                i++;
+            }
+        }
+    }
+
+    // Disable all obstacles on this tunnel cutout
+    public void DestroyObstacles()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            tunnelPieces[i].GetComponent<ObstacleGenerator>().DestroyObs();
+        }
+    }
+}
