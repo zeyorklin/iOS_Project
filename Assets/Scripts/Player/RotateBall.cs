71726d8afed351b892007502ab97dba6bb555165
Tony Zheyu Lin <tonyzeyorklin@gmail.com>
2018-04-16 21:30:57 -0400

    Minor movement change

--- Assets/Scripts/Player/RotateBall.cs
+++ Assets/Scripts/Player/RotateBall.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class RotateBall : MonoBehaviour {
 	public int rotatingSpeed = 0;
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate (new Vector3 (0, 0, rotatingSpeed) * Time.deltaTime);
+    private Rigidbody ball;
+
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+		//transform.Rotate (new Vector3 (0, 0, rotatingSpeed) * Time.deltaTime);
 	}
+
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, 0, rotatingSpeed) * Time.deltaTime);
+        if(Input.GetKey("left"))
+        {
+            transform.Rotate(new Vector3(0, -rotatingSpeed, 0) * Time.deltaTime);
+        }
+        else if(Input.GetKey("right"))
+        {
+            transform.Rotate(new Vector3(0, rotatingSpeed, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey("space"))
+        {
+            jump();
+        }
+    }
+    
+    void jump()
+    {
+        ball.AddForce(Vector3.up * 3 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
